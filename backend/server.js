b568a9297f7cc82fd61d3eef819e524ddd39b7f4
Ayushoopp <ayushmehrotra099@>
2023-05-02 14:41:43 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
+
+//COnfig
+dotenv.config({path: "backend/config/config.env"});
+
+//connection to database
+connectDatabase();
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is on http://localhost:${process.env.PORT}`)
+});
